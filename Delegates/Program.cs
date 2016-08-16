57e757abbfb28c5281f5a37ef80ceef8c8399490
Simflare <stu23@live.com>
2016-08-16 15:26:57 +0930

    Initial Commit

---
+++ Delegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate d;
+
+            d = DoSomething;
+
+            d("This is a delegate");
+
+            Console.ReadKey();
+        }
+
+        delegate void MyDelegate(string text);        
+        static public void DoSomething(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        
+
+       
+    }
+}
